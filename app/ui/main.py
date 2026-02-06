4c88fd45c3285b1e175f8df3481d8a20ab03a4ee
Sam Burney <sburney@sifnt.net.au>
2026-02-06 22:41:39 +1030

    WIP: Implementation of home page gallery.

--- app/ui/main.py
+++ app/ui/main.py
@@ -1,10 +1,12 @@
-from typing import Annotated
-from fastapi import APIRouter, Request, Depends
+from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
+from tortoise.expressions import Q
 
 from app.lib.config import get_app_config
 from app.lib.auth import get_current_user_from_request
 from app.ui.common import templates
+
+from app.models.uploads import Upload, UploadSerializer
 
 
 config = get_app_config()
@@ -17,4 +19,15 @@ async def index(request: Request):
     
     current_user = await get_current_user_from_request(request)
 
-    return templates.TemplateResponse(request, "index.html.j2", {"current_user": current_user})
+    # If user is logged, include their private uploads
+    # TODO: Make this a user configurable option
+    if current_user:
+        query = Q(private=False) | Q(user=current_user)
+    else:
+        query = Q(private=False)
+
+    # Get uploads
+    uploads_models = Upload.filter(query).order_by("-created_at").limit(24).prefetch_related("images")
+    uploads = await UploadSerializer.from_queryset(uploads_models)
+ 
+    return templates.TemplateResponse(request, "index.html.j2", {"current_user": current_user, "uploads": uploads})
